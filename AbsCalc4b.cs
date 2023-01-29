198ac322baae8f0a2be8cf9dcc813bcbfe0f5608
VigneshwarSJ <123438292+VigneshwarSJ@users.noreply.github.com>
2023-01-29 12:47:21 +0530

    Add files via upload

---
+++ AbsCalc4b.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Day4
+{
+
+    abstract class Calc
+    {
+        public abstract void add(int n1, int n2);
+        public abstract void sub(int n1, int n2);
+        public abstract void mul(int n1, int n2);
+        public abstract void div(int n1, int n2);
+    }
+
+    class ArithOper : Calc
+    {
+        public override void add(int n1, int n2)
+        {
+            int res = n1 + n2;
+            Console.WriteLine("num 1 + num 2 is : {0} ", res);
+        }
+        public override void sub(int n1, int n2)
+        {
+            int res = n1 - n2;
+            Console.WriteLine("num 1 - num 2 is : {0} ", res);
+        }
+        public override void mul(int n1, int n2)
+        {
+            int res = n1 * n2;
+            Console.WriteLine("num 1 * num 2 is : {0} ", res);
+        }
+        public override void div(int n1, int n2)
+        {
+            int res = n1 / n2;
+            Console.WriteLine("num 1 / num 2 is : {0} ", res);
+        }
+        internal class AbsCalc4b
+        {
+            public static void Main(string[] args)
+            {
+                ArithOper obj = new ArithOper();
+                Console.WriteLine("Enter n1:");
+                int n1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter n2:");
+                int n2 = Convert.ToInt32(Console.ReadLine());
+                obj.add(n1, n2);
+                obj.sub(n1, n2);
+                obj.mul(n1, n2);
+                obj.div(n1, n2);
+
+            }
+        }
+    }
+}
---
+++ AbsClass4a.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Day4
+{
+    abstract class member
+    {
+        public abstract void goldPack();
+        public abstract void silverPack();
+
+        public abstract void platinumPack();
+    }
+
+    class impPack : member
+    {
+        public override void goldPack()
+        {
+            Console.WriteLine("Gold pack contains five days accomodation at resort & two dinners on the house");
+
+        }
+        public override void silverPack()
+        {
+            Console.WriteLine("Silver pack contains three days accomodation at resort ");
+
+        }
+        public override void platinumPack()
+        {
+            Console.WriteLine("Platinum pack contains seven days accomodation at resort & five dinners on the house");
+
+        }
+
+
+    }
+    internal class AbsClass4a
+    {
+        public static void Main(string[] args)
+        {
+            impPack obj = new();
+           
+            Console.WriteLine("Choose your package:\n1.Gold 2.Silver 3.Platinum");
+            int pack = Convert.ToInt32(Console.ReadLine());
+                switch (pack)
+            {
+                case 1:
+                    obj.goldPack();
+                    break;
+                case 2:
+                    obj.silverPack();
+                    break;
+                case 3:
+                    obj.platinumPack();
+                    break;
+                default:
+                    Console.WriteLine("Select between 1 to 3");
+                    break;
+            }
+        }
+    }
+}
